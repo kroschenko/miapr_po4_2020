96ee4dea41afb9791efca7ba82fb2d9f250b3e3f
unknown <gribovskij.01@gmail.com>
2020-09-29 20:15:29 +0300

    Lab1

--- reports/Грибовский/lab1/src/main.py
+++ reports/Грибовский/lab1/src/main.py
@@ -4,7 +4,7 @@ import random
 a = 2
 b = 5
 d = 0.6
-L = 5
+input = 5
 
 alpha = 0.1
 Em = 0.0001
@@ -14,30 +14,30 @@ w = []
 
 print("Весовые коэффициенты:")
 
-for i in range(L):
+for i in range(input):
     w.append(random.random())
     print(w[i])
 
-m = 30
-m2 = 15
+n = 30
+n2 = 15
 et = []
-for i in range(m + m2):
+for i in range(n + n2):
     step = 0.1
     x = step * i
     et.append(a * math.sin(b * x) + d)
 
 while 1:
     E = 0
-    for i in range(m - L):
+    for i in range(n - input):
         y1 = 0
-        for j in range(L):
+        for j in range(input):
             y1 += w[j] * et[i + j]
         y1 -= T
 
-        for j in range(L):
-            w[j] -= alpha * (y1 - et[i + L]) * et[i + j]
-        T += alpha * (y1 - et[i + L])
-        E += 0.5 * ((y1 - et[i + L]) ** 2)
+        for j in range(input):
+            w[j] -= alpha * (y1 - et[i + input]) * et[i + j]
+        T += alpha * (y1 - et[i + input])
+        E += 0.5 * ((y1 - et[i + input]) ** 2)
 
     if E < Em:
         break
@@ -52,11 +52,11 @@ print(" %2s  %2s  %2s  %2s " % (
     ))
 
 training = []
-for i in range(m):
+for i in range(n):
     training.append(0)
 
-    for j in range(L):
-        training[i] += w[j] * et[j + i - L]
+    for j in range(input):
+        training[i] += w[j] * et[j + i - input]
     training[i] -= T
 
     print(" %2d  %9lf  %18lf  %19lf " % (
@@ -74,17 +74,17 @@ print(" %2s  %2s  %2s  %2s " % (
         "Отклонение"
     ))
 
-for i in range(m2):
+for i in range(n2):
     training.append(0)
 
-    for j in range(L):
-        training[i + m] += w[j] * et[m - L + j + i]
+    for j in range(input):
+        training[i + n] += w[j] * et[n - input + j + i]
 
-    training[i + m] -= T
+    training[i + n] -= T
 
     print(" %2d  %9lf  %18lf  %19lf " % (
-        i + m,
-        et[i + m],
-        training[i + m],
-        et[i + m] - training[i + m]
+        i + n,
+        et[i + n],
+        training[i + n],
+        et[i + n] - training[i + n]
     ))
