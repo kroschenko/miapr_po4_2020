7ffdbe924c8d8d22c45d0e15e013372811913616
unknown <gribovskij.01@gmail.com>
2020-10-13 17:23:26 +0300

    Lab2

--- reports/Gribovskij/2/src/lab2.py
+++ reports/Gribovskij/2/src/lab2.py
@@ -1,16 +1,22 @@
 ﻿import math
 import random
 
+def function(x, a, b, d):
+    return a * math.sin(b * x) + d
+
 a = 2
 b = 5
 d = 0.6
-inputs = 5
 
-alpha = 0.8
-Em = 1e-6
+inputs = 5               
+Em = 1e-8               
+step = 0.1                          
+alpha = 0.1                              
 
-T = 0.6 
-w = []
+T = random.uniform(0.5, 1)       
+
+w = []              
+
 
 print("Весовые коэффициенты:")
 
@@ -18,72 +24,74 @@ for i in range(inputs):
     w.append(random.random())
     print(w[i])
 
-n = 30
-n2 = 15
-et = []
+
+et = []             
+n = 30       
+n2 = 15    
 for i in range(n + n2):
-    step = 0.1
-    x = step * i
-    et.append(a * math.sin(b * x) + d)
+    x = i * step
+    et.append(function(x, a, b, d))
 
 count = 0
 
 while 1:
+
     E = 0
 
-    for i in range(n - inputs):
-
-        
-        y1 = 0
-
-        for j in range(inputs):
-            y1 += w[j] * et[i + j]
-        y1 -= T
-        
-        for j in range(inputs):
-            w[j] -= alpha * (y1 - et[i + inputs]) * et[i + j]
-        T += alpha * (y1 - et[i + inputs])
-        E += 0.5 * ((y1 - et[i + inputs]) ** 2)
+    for i in range(n):
 
         temp = 0
         for j in range(inputs):
-            temp += math.pow(et[i + j], 2)
-        alpha = 1 / (1 + temp) 
+           temp += (et[i + j])**2
+        alpha = 1/(1 + temp)
 
-    count += 1
-    
-    print("Error: ", E)
+        y = 0
+
+        for j in range(inputs):
+            y += (w[j] * et[j + i])
+
+        y -= T
+
+        for j in range(inputs):
+            w[j] -= alpha * (y - et[i + inputs]) * et[i + j]
+        T += alpha * (y - et[i + inputs]) 
+        E += 0.5 * ((y - et[i + inputs]) ** 2)
+
     if E < Em:
         break
+    
+    print("Error: ", E)
+    count += 1
 
-print("Эпохи", count)
+print("Эпохи ", count)
 
-print("\nРезультат тренировки:\n")
-print(" %2s  %2s  %2s  %2s " % (
+training = []
+
+print("Результаты обучения:")
+print(" %2s %2s %2s %2s " % (
         "y[]",
         "Эталонное значение",
         "Полученное значение",
         "Отклонение"
     ))
 
-training = []
-
 for i in range(n):
     training.append(0)
 
     for j in range(inputs):
         training[i] += w[j] * et[j + i]
+
     training[i] -= T
 
-    print(" %2d  %9lf  %18lf  %19lf " % (
-        i,
-        et[i + inputs],
-        training[i],
-        training[i] - et[i + inputs]
-    ))
+    print(" %2d %9lf %18lf %19lf " % (
+            i,
+            et[i + inputs],
+            training[i],
+            training[i] - et[i + inputs]
+        ))
 
-print("\nРезультат прогназированния:\n")
-print(" %2s  %2s  %2s  %2s " % (
+print("Результаты прогнозирования:")
+print(" %2s %2s %2s %2s " % (
         "y[]",
         "Эталонное значение",
         "Полученное значение",
@@ -95,11 +103,17 @@ for i in range(n2):
 
     for j in range(inputs):
         training[i + n] += w[j] * et[n - inputs + j + i]
+
     training[i + n] -= T
 
-    print(" %2d  %9lf  %18lf  %19lf " % (
-        i + n,
-        et[i + n],
-        training[i + n],
-        training[i + n] - et[i + n]
-    ))
+    print(" %2d %9lf %18lf %19lf " % (
+            i + n,
+            training[i + n],
+            et[i + n],
+            training[i + n] - et[i + n]
+        ))
+
+
+
+
+
