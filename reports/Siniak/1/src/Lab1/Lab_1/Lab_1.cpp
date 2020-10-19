83d8f6085723faccabf8d6f45bc8661c8842fd35
DanilaSiniak <sinyak2002@gmail.com>
2020-10-19 22:43:57 +0300

    Fixed lab 1, add lab 2

--- reports/Siniak/1/src/Lab1/Lab_1/Lab_1.cpp
+++ reports/Siniak/1/src/Lab1/Lab_1/Lab_1.cpp
@@ -1,100 +1,90 @@
 ﻿#include <iostream>
+#include <Windows.h>
 #include <iomanip>
+#include <ctime>
+#include <cmath>
 
 using namespace std;
 
-int main()
-{
-	setlocale(LC_ALL, "Russian");
+double function(int a, int b, double x, double d);
+void print_result(int n, double T, double* Y, int n_learn, int N, int n_protected, double* W);
+
+int main() {
+	SetConsoleOutputCP(1251);
 	system("color f0");
-	int a = 4,
-		b = 7,
-		num_enteries = 4, //количество входов ИНС
-		n = 30, //количесвто значений, на которых производится обучение
-		num_predicated_values = 15; //количесвто значений, на которых производится прогнозирование
-
-	double d = 0.2,
-		Em = 0.01, //минимальная среднеквадратичная ошибка сети
-		E, //суммарная среднеквадратичная ошибка
-		T = 1; //порог нейронной сети
-
-	double* W = new double[num_enteries]; //весовые коэффициенты
-	for (int i = 0; i < num_enteries; i++) 
-	{ //задаем случайным образом весовые коэффициенты
-		W[i] = static_cast <double> (rand()) / (static_cast <double>(RAND_MAX / 10));
+	srand(time(0));
+	int a = 4, b = 7, n = 5, n_learn = 30, n_predicted = 15;
+	double d = 0.2, step = 0.1, x1 = 0, E, Em = 0.001, T = 1;
+	double* W = new double[n];
+	for (int i = 0; i < n; i++) {
+		W[i] = 1.0 / (double)rand();
 	}
-
-	double* reference_value_y = new double[n + num_predicated_values]; //эталонные значения y
-	for (int i = 0; i < n + num_predicated_values; i++) //вычисляем эталонные значения
-	{ 
-		double step = 0.1, //шаг
-			x = step * i;
-		reference_value_y[i] = a * sin(b * x) + d;
+	int N = n_learn + n_predicted;
+	double* Y = new double[N];
+	for (int i = 0; i < N; i++) {
+		x1 += step;
+		Y[i] = function(a, b, x1, d);
 	}
-
-	do 
-	{
-		double y1, //выходное значение нейронной сети
-			A = 0.0005; //скорость обучения
+	double y1, //выходное значение нейронной сети
+		alpha; //скорость обучения
+	do {
 		E = 0;
-
-		for (int i = 0; i < n - num_enteries; i++) 
-		{
+		for (int i = 0; i < n_learn - n; i++) {
 			y1 = 0;
 
-			for (int j = 0; j < num_enteries; j++)//векторы выходной активности сети
-			{ 
-				y1 += W[j] * reference_value_y[i + j];
+			double x2 = 0.0;
+			for (int j = 0; j < n; j++) {
+				x2 += pow(Y[i + j], 2);
+			}
+			alpha = 1 / (1 + x2); //адаптивный шаг
+
+			for (int j = 0; j < n; j++) { //векторы выходной активности сети
+				y1 += W[j] * Y[i + j];
 			}
 			y1 -= T;
 
-			for (int j = 0; j < num_enteries; j++) //изменение весовых коэффициентов
-			{ 
-				W[j] -= A * (y1 - reference_value_y[i + num_enteries]) * reference_value_y[i + j];
+			for (int j = 0; j < n; j++) { //изменение весовых коэффициентов
+				W[j] -= alpha * (y1 - Y[i + n]) * Y[i + j];
 			}
 
-			T += A * (y1 - reference_value_y[i + num_enteries]); //изменение порога нейронной сети
-			E += 0.5 * pow(y1 - reference_value_y[i + num_enteries], 2); //расчет суммарной среднеквадратичной ошибки
-
+			T += alpha * (y1 - Y[i + n]); //изменение порога нейронной сети
+			E += 0.5 * pow(y1 - Y[i + n], 2); //расчет суммарной среднеквадратичной ошибки
 		}
 	} while (E > Em);
-
-	cout << "Результаты обучения" << endl;
-	cout << setw(27) << left << "Эталонные значения" << setw(23) << left << "Полученные значения" << "Отклонение" << endl;
-	double* predicated_values = new double[n + num_predicated_values];
-
-	for (int i = 0; i < n; i++)
-	{
-		predicated_values[i] = 0;
-		for (int j = 0; j < num_enteries; j++)
-		{
-			predicated_values[i] += W[j] * reference_value_y[j + i - num_enteries]; //получаемые значения в результате обучения
-		}
-		predicated_values[i] -= T;
-
-		cout << "y[" << i << "] = " << setw(20) << left << reference_value_y[i] << setw(23) << left;
-		cout << predicated_values[i] << reference_value_y[i] - predicated_values[i] << endl;
-	}
-
-	cout << endl << "Результаты прогнозирования" << endl;
-	cout << setw(28) << left << "Эталонные значения" << setw(23) << left << "Полученные значения" << "Отклонение" << endl;
-
-	for (int i = 0; i < num_predicated_values; i++) {
-		predicated_values[i + n] = 0;
-		for (int j = 0; j < num_enteries; j++) {
-			//прогнозируемые значения
-			predicated_values[i + n] += W[j] * reference_value_y[n - num_enteries + j + i];
-		}
-		predicated_values[i + n] += T;
-
-		cout << "y[" << n + i << "] = " << setw(20) << left << reference_value_y[i + n] << setw(23) << left;
-		cout << predicated_values[i + n] << reference_value_y[i + n] - predicated_values[i + n] << endl;
-	}
-
-	delete[]reference_value_y;
-	delete[]predicated_values;
-	delete[]W;
-
-	system("pause");
+	print_result(n, T, Y, n_learn, N, n_predicted, W);
+	delete[] Y;
+	delete[] W;
 	return 0;
+}
+
+double function(int a, int b, double x, double d)
+{
+	return a * sin(b * x) + d;
+}
+
+void print_result(int n, double T, double* Y, int n_learn, int N, int n_predicted, double* W)
+{
+	cout << "РЕЗУЛЬТАТЫ:" << endl;
+	cout << "1) Обучение:" << endl;
+	cout << setw(30) << left << "Эталонные значения" << setw(38) << left << "Полученные значения" << "Отклонение" << endl;
+	double* prediction = new double[N];
+	for (int i = 0; i < n_learn; i++) {
+		prediction[i] = 0;
+		for (int j = 0; j < n; j++) {
+			prediction[i] += W[j] * Y[j + i];
+		}
+		prediction[i] -= T;
+		cout << "y[" << i << "] = " << setw(30) << left << Y[i + n] << setw(30) << left << prediction[i] << pow(Y[i + n] - prediction[i], 2) << endl;
+	}
+	cout << "2) Прогнозирование:" << endl;
+	cout << setw(30) << left << "Эталонные значения" << setw(38) << left << "Полученные значения" << "Отклонение" << endl;
+	for (int i = 0; i < n_predicted; i++) {
+		prediction[i + n_learn] = 0;
+		for (int j = 0; j < n; j++) {
+			prediction[i + n_learn] += W[j] * Y[i + j + n_learn - n];
+		}
+		prediction[i + n_learn] -= T;
+		cout << "y[" << i + n_learn << "] = " << setw(30) << left << Y[i + n_learn] << setw(30) << left << prediction[i + n_learn] << pow(Y[i + n_learn] - prediction[i + n_learn], 2) << endl;
+	}
+	delete[] prediction;
 }
