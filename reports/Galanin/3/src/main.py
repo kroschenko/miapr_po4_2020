7cb24b8bbd3a4b1a098319a8d1dccb334b44370c
PavelGalanin2001 <PavelGalanin2001@outlook.com>
2020-11-04 00:39:26 +0300

    update cycle

--- reports/Galanin/3/src/main.py
+++ reports/Galanin/3/src/main.py
@@ -8,23 +8,23 @@ from sigm import sigm
 L = 8 # Количество входов ИНС
 Lhid = 3 # Количество НЭ скрытого слоя
 
-e = get_etalons(L) # эталонные значения
+e = get_etalons(30) # эталонные значения
 
 w = np.zeros((L, Lhid)) # массив размером [L x Lhid] для весов от входа к скрытому слою
 
 for i in range(L):
     for j in range(Lhid):
-        w[i][j] = random.uniform(-1, 1)
+        w[i][j] = random.uniform(0, 1)
 
 ws = np.zeros(Lhid) # массив размером [Lhid x 1] для весов от скрытого слоя к выходу
 for i in range(Lhid):
-    w[i] = random.uniform(-1, 1)
+    w[i] = random.uniform(0, 1)
 
 T = np.zeros(Lhid) # пороги от входов к срытому слою
 for i in range(Lhid):
-    T[i] = random.uniform(-1, 1)
+    T[i] = random.uniform(0, 1)
 
-Ts = random.uniform(-1, 1) # порог от скрытого слоя к выходу
+Ts = random.uniform(0, 1) # порог от скрытого слоя к выходу
 
 Ee = 1e-6 # желаемая квадратичная ошибка, до значения которой мы хотим обучить сеть
 
@@ -33,53 +33,55 @@ alpha = 0.5 # 0 < alpha < 1 - скорость обучения
 x = np.zeros(L)
 for i in range(L):
     x[i] = e[i]
-print('x =\n%s\n' % x)
-print('w =\n%s\n' % w)
+#print('x =\n%s\n' % x)
+#print('w =\n%s\n' % w)
 
 eras = 0
 valueXforGraph = []
 valueYforGraph = []
 while 1:
-    S = x.dot(w) - T # S = [x] * [w] - [T] 
-    print('S = x * w - T =\n%s\n' % S)
+    for k in range(30 - Lhid):
+        S = x.dot(w) - T # S = [x] * [w] - [T] 
+        #print('S = x * w - T =\n%s\n' % S)
 
-    for i in range(Lhid):
-        S[i] = sigm(S[i]) # [sigm(S1), sigm(S2), ..., sigm(SLhid)]
-    y = S
-    print('y = [sigm(S1), ..., sigm(SLhid)] =\n%s\n' % y)
+        for i in range(Lhid):
+            S[i] = sigm(S[i]) # [sigm(S1), sigm(S2), ..., sigm(SLhid)]
+        y = S
+        print('y = [sigm(S1), ..., sigm(SLhid)] =\n%s\n' % y)
 
-    Y = y.dot(ws) - Ts # Y = [y] * [ws] - [Ts]
-    print('Y = [y] * [ws] - [Ts] =\n%s\n' % Y)
+        Y = y.dot(ws) - Ts # Y = [y] * [ws] - [Ts]
+        #print('Y = [y] * [ws] - [Ts] =\n%s\n' % Y)
 
-    gamma = Y - e[0]
-    print('gamma =\n%s\n' % gamma)
+        gamma = Y - e[k]
+        #print('gamma =\n%s\n' % gamma)
 
-    ws = ws - alpha * gamma * 1 * y # [ws] = [ws] - a * j * 1 * Y
-    print('ws =\n%s\n' % ws)
+        ws = ws - alpha * gamma * 1 * y # [ws] = [ws] - a * j * 1 * Y
+        #print('ws =\n%s\n' % ws)
 
-    Ts = Ts + alpha * gamma * 1 # [Ts] = [Ts] + a * j * 1
-    print('Ts =\n%s\n' % Ts)
+        Ts = Ts + alpha * gamma * 1 # [Ts] = [Ts] + a * j * 1
+        #print('Ts =\n%s\n' % Ts)
 
-    gamma_s = np.zeros(Lhid)
-    for i in range(Lhid):
-        gamma_s[i] = y[i] - e[i]
+        gamma_s = np.zeros(Lhid)
+        for i in range(Lhid):
+            gamma_s[i] = y[i] - e[k + i]
 
-    for i in range(L):
-        for j in range(Lhid):
-            w[i][j] -= alpha * gamma_s[j] * y[j] * (1 - y[j]) * y[j]
-    print('w = w - a * j * y * (1-y) * y =\n%s\n' % w)
+        for i in range(L):
+            for j in range(Lhid):
+                w[i][j] -= alpha * gamma_s[j] * y[j] * (1 - y[j]) * y[j]
+        #print('w = w - a * j * y * (1-y) * y =\n%s\n' % w)
 
-    for i in range(Lhid):
-        T[i] += alpha * gamma_s[i] * y[i] * (1 - y[i])
-    print('T = T + alpha * j * y * (1-y) =\n%s\n' % T)
+        for i in range(Lhid):
+            T[i] += alpha * gamma_s[i] * y[i] * (1 - y[i])
+        #print('T = T + alpha * j * y * (1-y) =\n%s\n' % T)
 
-    E = 0.5 * (Y - e[0]) ** 2
-    print('E = %s' % E)
+        E = 0.5 * (Y - e[k]) ** 2
+        #print('E = %s' % E)
 
     eras += 1
 
     valueXforGraph.append(eras)
     valueYforGraph.append(E)
+    print('%24s %24s' % (eras, E))
 
     if E < Ee:
         break
