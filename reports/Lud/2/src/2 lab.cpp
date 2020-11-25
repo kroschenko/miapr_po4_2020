3d9c57152ab17d6001ca358a03c86c93ebfa845a
Aliaksandr Kroshchanka <kroschenko@gmail.com>
2020-11-25 07:41:51 +0300

    Fix repository structure

---
+++ reports/Lud/2/src/2 lab.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int a = 4,
+        b = 8,
+        inputs_number = 3, // входы ИНС
+        n_obychenie = 30, // кол зн на кот проводится обучение
+        n_prognoz = 15, // кол зн на кот проводится прогнозирование
+        era = 0;
+
+    double d = 0.4,
+        x = 0,
+        h = 0.1, // шаг
+        min_error = 0.001, //минимальная ошибка
+        sum_error, //суммарная ошибка
+        T = 1; // пороговое значение ИНС
+
+    double* w = new double[inputs_number]; // весовые коэффициенты
+    for (int i = 0; i < inputs_number; i++) { // задаём их с помощью рандома
+        w[i] = rand() % 100 * 0.1;
+    }
+
+    double* etalon = new double[n_obychenie + n_prognoz]; // эланонные зн для у
+    for (int i = 0; i < n_obychenie + n_prognoz; i++) { // вычисление эталонных значений
+        x += h;
+        etalon[i] = a * sin(b * x) + d;
+    }
+
+    double y, // вых зн ИНС
+           V; // скорость
+    do {
+        sum_error = 0;
+        for (int i = 0; i < n_obychenie - inputs_number; i++) {
+            y = 0;
+
+            double t = 0.0;
+            for (int j = 0; j < inputs_number; j++) {
+                t += pow(etalon[i + j], 2);
+            }
+            V = 1 / (1 + t); // адаптивный шаг
+
+            for (int j = 0; j < inputs_number; j++) {
+                y += w[j] * etalon[i + j];
+            }
+            y -= T;
+            for (int j = 0; j < inputs_number; j++) {
+                w[j] -= V * (y - etalon[i + inputs_number]) * etalon[i + j];
+            }
+            sum_error += 0.5 * pow((y - etalon[i + inputs_number]), 2);
+            T += V * (y - etalon[i + inputs_number]);     
+        }
+        era++;
+    } while (sum_error > min_error);
+
+    cout << "Количество эпох: " << era << endl;
+    cout << "||Результаты Обучения||" << endl;
+    cout << setw(27) << left << "|Эталонные значения|" << setw(29) << left << "|Полученные значения|" << setw(30) << left << "|Отклонение|" << endl;
+
+    double* prognoz = new double[n_obychenie + n_prognoz];
+    for (int i = 0; i < n_obychenie; i++) { //значения в результате обучения
+        prognoz[i] = 0;
+        for (int j = 0; j < inputs_number; j++) {
+            prognoz[i] += w[j] * etalon[i + j];
+        }
+        prognoz[i] -= T;
+
+        cout << "y[" << i << "] = " << setw(25) << left << etalon[i + inputs_number] << setw(25) << left;
+        cout << prognoz[i] << setw(30) << left << pow(etalon[i + inputs_number] - prognoz[i], 2) << endl;
+    }
+
+    cout << "||Результаты Прогнозирования||" << endl;
+    cout << setw(27) << left << "|Эталонные значения|" << setw(29) << left << "|Полученные значения|" << setw(30) << left << "|Отклонение|" << endl;
+
+    for (int i = 0; i < n_prognoz; i++) { //прогнозируемые значения
+        prognoz[i + n_obychenie] = 0;
+        for (int j = 0; j < inputs_number; j++) {
+            prognoz[i + n_obychenie] += w[j] * etalon[n_obychenie + j + i - inputs_number];
+        }
+        prognoz[i + n_obychenie] -= T;
+
+        cout << "y[" << n_obychenie + i << "] = " << setw(25) << left << etalon[i + n_obychenie] << setw(25) << left;
+        cout << prognoz[i + n_obychenie] << setw(30) << left << pow(etalon[i + n_obychenie] - prognoz[i + n_obychenie], 2) << endl;
+    }
+    delete[] w;
+    delete[]etalon;
+    delete[]prognoz;
+    system("pause");
+    return 0;
+}
+
+
---
+++ reports/Mayevski/1/src/Source.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <Windows.h>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+double function(int a, int b, double x, double d);
+void print_result(int n, double T, double* Y, int Num_Learning, int N, int n_protected, double* W);
+
+int main() {
+	SetConsoleOutputCP(1251);
+	system("color f0");
+	srand(time(0));
+	int a = 1, b = 9, n = 4, Num_Learning = 30, Num_Predicted = 15;
+	double d = 0.5, step = 0.1, x = 0, E, Em = 0.001, T = 1;
+	int epox = 0;
+	double* W = new double[n];
+	for (int i = 0; i < n; i++) {
+		W[i] = 1.0 / (double)rand();
+	}
+	int N = Num_Learning + Num_Predicted;
+	double* Y = new double[N];
+	for (int i = 0; i < N; i++) {
+		x += step;
+		Y[i] = function(a, b, x, d);
+	}
+	double y1, //выходное значение нейронной сети
+		step_learning = 0.3; //скорость обучения
+	do {
+		E = 0;
+		for (int i = 0; i < Num_Learning - n; i++) {
+			y1 = 0;
+
+			for (int j = 0; j < n; j++) { //векторы выходной активности сети
+				y1 += W[j] * Y[i + j];
+			}
+			y1 -= T;
+
+			for (int j = 0; j < n; j++) { //изменение весовых коэффициентов
+				W[j] -= step_learning * (y1 - Y[i + n]) * Y[i + j];
+			}
+
+			T += step_learning * (y1 - Y[i + n]); //изменение порога нейронной сети
+			E += 0.5 * pow(y1 - Y[i + n], 2); //расчет суммарной среднеквадратичной ошибки
+		}		
+		epox++;
+		
+	} while (E > Em);
+	cout <<"количество эпох: "  << epox;
+	print_result(n, T, Y, Num_Learning, N, Num_Predicted, W);
+	delete[] Y;
+	delete[] W;
+	system("pause");
+	return 0;
+	
+}
+
+double function(int a, int b, double x, double d)
+{
+	return a * sin(b * x) + d;
+}
+
+void print_result(int n, double T, double* Y, int Num_Learning, int N, int Num_Predicted, double* W)
+{
+	cout << "РЕЗУЛЬТАТЫ:" << endl;
+	cout << "1) Обучение:" << endl;
+	cout << setw(30) << left << "Эталонные значения" << setw(38) << left << "Полученные значения" << "Отклонение" << endl;
+	double* predict = new double[N];
+	for (int i = 0; i < Num_Learning; i++) {
+		predict[i] = 0;
+		for (int j = 0; j < n; j++) {
+			predict[i] += W[j] * Y[j + i];
+		}
+		predict[i] -= T;
+		cout << "y[" << i << "] = " << setw(30) << left << Y[i + n] << setw(30) << left << predict[i] << pow(Y[i + n] - predict[i], 2) << endl;
+	}
+	cout << "2) Прогнозирование:" << endl;
+	cout << setw(30) << left << "Эталонные значения" << setw(38) << left << "Полученные значения" << "Отклонение" << endl;
+	for (int i = 0; i < Num_Predicted; i++) {
+		predict[i + Num_Learning] = 0;
+		for (int j = 0; j < n; j++) {
+			predict[i + Num_Learning] += W[j] * Y[i + j + Num_Learning - n];
+		}
+		predict[i + Num_Learning] -= T;
+		cout << "y[" << i + Num_Learning << "] = " << setw(30) << left << Y[i + Num_Learning] << setw(30) << left << predict[i + Num_Learning] << pow(Y[i + Num_Learning] - predict[i + Num_Learning], 2) << endl;
+	}
+	delete[] predict;
+}
