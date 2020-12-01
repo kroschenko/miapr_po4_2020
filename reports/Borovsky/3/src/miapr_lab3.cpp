3faae29f87e957dbae699eea1524ae4aa678b1e1
MrGodSmile <mrgodsmile@yandex.ru>
2020-12-01 13:19:39 +0300

    lab3_fix

--- reports/Borovsky/3/src/miapr_lab3.cpp
+++ reports/Borovsky/3/src/miapr_lab3.cpp
@@ -1,86 +1,98 @@
 #include <iostream>
 #include <math.h>
 #include <iomanip>
+#define input_layer 10
+#define hidden_layer 4
 
 using namespace std;
 
-double Sigmoid(double x) {
-	return 1 / (1 + pow(2, -x));
+double sigmoid(double x)
+{
+	return 1 / (1 + pow(2.7, -x));
 }
 
-double func(double x) {
+double function(double x)
+{
 	double a = 0.3, b = 0.3, c = 0.07, d = 0.3;
 	return a * cos(b * x) + c * sin(d * x);
 }
 
-double* hidden(double x, double Wes1[4][10], double T[4]) {
-	double* result_value = new double[4];
-	for (int i = 0; i < 4; i++) {
-		result_value[i] = 0;
+double* hidden(double x, double w1[hidden_layer][input_layer], double* T)
+{
+	double* result = new double[hidden_layer];
+	for (int i = 0; i < hidden_layer; i++)
+		result[i] = 0;
+	double Inputs[input_layer];
+	for (int k = 0; k < input_layer; k++, x += 0.1)
+		Inputs[k] = function(x);
+	for (int i = 0; i < hidden_layer; i++)
+	{
+		for (int k = 0; k < input_layer; k++)
+			result[i] += Inputs[k] * w1[i][k];
+		result[i] -= T[i];
+		result[i] = sigmoid(result[i]);
 	}
-	double entrances[10];
-	for (int k = 0; k < 10; k++, x += 0.1) {
-		entrances[k] = func(x);
-	}
-	for (int i = 0; i < 4; i++) {
-		for (int k = 0; k < 10; k++) {
-			result_value[i] += entrances[k] * Wes1[i][k];
-		}
-		result_value[i] -= T[i];
-		result_value[i] = Sigmoid(result_value[i]);
-	}
-	return result_value;
-}
-double output(double x, double Wes1[4][10], double Wes2[4], double T[4 + 1]) {
-	double result = 0;
-	double* hidden_result_value = hidden(x, Wes1, T);
-	for (int j = 0; j < 4; j++) {
-		result += hidden_result_value[j] * Wes2[j];
-	}
-	result -= T[4];
 	return result;
 }
-int main() {
-	setlocale(0, "");
-	double Wes1[4][10], Wes2[4], T[4 + 1], reference_value, E_min = 0.00002, alpha = 0.4, x = 4, current, E = 0;
-	for (int i = 0; i < 4; i++) {
-		for (int k = 0; k < 10; k++) {
-			Wes1[i][k] = ((double)rand() / RAND_MAX);
-		}
-		Wes2[i] = ((double)rand() / RAND_MAX);
-		T[i] = ((double)rand() / RAND_MAX);
+
+double output(double x, double w1[hidden_layer][input_layer], double* w2, double* T)
+{
+	double Result = 0;
+	double* hidden_result = hidden(x, w1, T);
+	for (int j = 0; j < hidden_layer; j++) {
+		Result += hidden_result[j] * w2[j];
 	}
-	T[4] = ((double)rand() / RAND_MAX);
+	Result -= T[2];
+	return Result;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int epox = 0;
+	double w1[hidden_layer][input_layer], w2[hidden_layer], T[hidden_layer + 1], Reference, E_min = 0.00002, alpha = 0.4, x = 4, current, E = 0;
+	for (int i = 0; i < hidden_layer; i++)
+	{
+		for (int k = 0; k < input_layer; k++)
+		{
+			w1[i][k] = ((double)rand() / RAND_MAX) * 0.05;
+		}
+		w2[i] = ((double)rand() / RAND_MAX) * 0.05;
+		T[i] = ((double)rand() / RAND_MAX) * 0.05;
+	}
+	T[2] = ((double)rand() / RAND_MAX) * 0.05;
 	do
 	{
 		E = 0;
-		for (int q = 0; q < 200; q++)
+		for (int q = 0; q < 350; q++)
 		{
-			current = output(x, Wes1, Wes2, T);
-			reference_value = func(x + 10 * 0.1);
-			double Error = current - reference_value;
-			double* Hiddens = hidden(x, Wes1, T);
-			for (int j = 0; j < 4; j++)
-				Wes2[j] -= alpha * Error * Hiddens[j];
-			T[3] += alpha * Error;
-			for (int k = 0; k < 4; k++)
+			current = output(x, w1, w2, T);
+			Reference = function(x + 10 * 0.1);
+			double error = current - Reference;
+			double* Hiddens = hidden(x, w1, T);
+			for (int j = 0; j < hidden_layer; j++)
+				w2[j] -= alpha * error * Hiddens[j];
+			T[2] += alpha * error;
+			for (int k = 0; k < hidden_layer; k++)
 			{
-				for (int i = 0; i < 10; i++)
-					Wes1[k][i] -= alpha * func(x + i * 0.1) * Hiddens[k] * (1 - Hiddens[k]) * Wes2[k] * Error;
-				T[k] += alpha * Hiddens[k] * (1 - Hiddens[k]) * Wes2[k] * Error;
+				for (int i = 0; i < input_layer; i++)
+					w1[k][i] -= alpha * function(x + i * 0.1) * Hiddens[k] * (1 - Hiddens[k]) * w2[k] * error;
+				T[k] += alpha * Hiddens[k] * (1 - Hiddens[k]) * w2[k] * error;
 			}
 			x += 0.1;
-			E += pow(Error, 4);
+			E += pow(error, 2);
 		}
-		E /= 4;
-		cout << "Error: " << E << endl;
+		E /= 2;
+		cout << "Error " << E << endl;
+		epox++;
 	} while (E > E_min);
-	cout << "N" << "\t" << "Эталон" << setw(20) << "Получ.знач." << setw(15) << "Отклонение" << endl;
-	for (int i = 0; i < 15; i++) {
-		double result = output(x, Wes1, Wes2, T), ethelon_value = func(x + 10 * 0.1);
-		cout << i + 1 << "\t" << ethelon_value << right << setw(15) << result << right << setw(15) << result - ethelon_value << endl;
+	cout << epox << endl;
+	cout << "Эталон" << setw(23) << "Прогноз" << setw(20) << "Отклонение" << endl;
+	for (int i = 0; i < 100; i++)
+	{
+		double Result = output(x, w1, w2, T), Ethalonn = function(x + 10 * 0.1);
+		cout << fixed << setprecision(5) << Ethalonn << setw(21) << Result << setw(29) << Result - Ethalonn << endl;
 		x += 0.1;
 	}
 	system("pause");
-	return 0;
 }
