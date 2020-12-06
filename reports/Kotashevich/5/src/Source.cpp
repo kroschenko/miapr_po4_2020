fa17849fec49593f9dc6c3803a51f9d16fbcab2d
cneker <staskotashevich@gmail.com>
2020-12-06 18:25:27 +0300

    fix lab5

--- reports/Kotashevich/5/src/Source.cpp
+++ reports/Kotashevich/5/src/Source.cpp
@@ -1,4 +1,6 @@
-#include <iostream> 
+﻿#include <iostream>
+#define INPUT 6
+#define HIDDEN 2
 using namespace std;
 
 double sigmoid(double x)
@@ -6,13 +8,13 @@ double sigmoid(double x)
 	return 1 / (1 + pow(2.7, -x));
 }
 
-double* getHidden(bool* Inputs, double W12[20][40], double THid[])
+double* getHidden(bool* Inputs, double W12[INPUT][HIDDEN], double THid[])
 {
-	double* hidden = new double[40];
-	for (int i = 0; i < 40; i++) hidden[i] = 0;
-	for (int i = 0; i < 40; i++)
+	double* hidden = new double[HIDDEN];
+	for (int i = 0; i < HIDDEN; i++) hidden[i] = 0;
+	for (int i = 0; i < HIDDEN; i++)
 	{
-		for (int j = 0; j < 20; j++)
+		for (int j = 0; j < INPUT; j++)
 		{
 			hidden[i] += W12[j][i] * Inputs[j];
 		}
@@ -22,14 +24,14 @@ double* getHidden(bool* Inputs, double W12[20][40], double THid[])
 	return hidden;
 }
 
-double* getResult(bool* Inputs, double W12[20][40], double THid[], double W23[40][3], double TOut[], double hidden[40])
+double* getResult(bool* Inputs, double W12[INPUT][HIDDEN], double THid[], double W23[HIDDEN][3], double TOut[], double hidden[HIDDEN])
 {
 	double* Results = new double[3];
 	for (int i = 0; i < 3; i++)
 		Results[i] = 0;
 	for (int j = 0; j < 3; j++)
 	{
-		for (int i = 0; i < 40; i++)
+		for (int i = 0; i < HIDDEN; i++)
 		{
 			Results[j] += hidden[i] * W23[i][j];
 		}
@@ -46,22 +48,22 @@ int main()
 	bool Vect1[] = { 0,1,0,0,1,1,0,1,0,0,0,0,1,0,1,0,1,0,0,0 };
 	bool Vect2[] = { 1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0 };
 	bool Vect3[] = { 1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1 };
-	bool* Inputs = new bool[20];
-	for (int i = 0; i < 20; i++) Inputs[i] = 0;
+	bool* Inputs = new bool[INPUT];
+	for (int i = 0; i < INPUT; i++) Inputs[i] = 0;
 	bool** Vectors = new bool* [12];
 	Vectors[0] = Vect1;
 	Vectors[1] = Vect2;
 	Vectors[2] = Vect3;
-	double W12[20][40], W23[40][3], THid[40], TOut[3], E_min = 0.001, alpha = 0.04, Ethalon, E = 0, Outputs[3] = { 0 };
+	double W12[INPUT][HIDDEN], W23[HIDDEN][3], THid[HIDDEN], TOut[3], E_min = 0.001, alpha = 0.04, Ethalon, E = 0, Outputs[3] = { 0 };
 	double* Currents = new double[3];
-	double* hidden = new double[40];
+	double* hidden = new double[HIDDEN];
 	double Mistakes[3] = { 0 };
 	double Ethalons[3] = { 0 };
-	double MistakesHid[40] = { 0 };
+	double MistakesHid[HIDDEN] = { 0 };
 
-	for (int i = 0; i < 20; i++)
+	for (int i = 0; i < INPUT; i++)
 	{
-		for (int j = 0; j < 40; j++)
+		for (int j = 0; j < HIDDEN; j++)
 		{
 			W12[i][j] = ((double)rand() / (RAND_MAX)) - 0.5;
 			for (int k = 0; k < 3; k++)
@@ -85,7 +87,7 @@ int main()
 			for (int i = 0; i < 3; i++)
 				Mistakes[i] = Currents[i] - Ethalons[i];
 
-			for (int j = 0; j < 40; j++)
+			for (int j = 0; j < HIDDEN; j++)
 			{
 				for (int m = 0; m < 3; m++) {
 					MistakesHid[j] += Mistakes[m] * Currents[m] * (1 - Currents[m]) * W23[j][m];
@@ -93,16 +95,16 @@ int main()
 			}
 			for (int j = 0; j < 3; j++)
 			{
-				for (int i = 0; i < 40; i++)
+				for (int i = 0; i < HIDDEN; i++)
 				{
 					W23[i][j] -= alpha * Mistakes[j] * Currents[j] * (1 - Currents[j]) * hidden[i];
 				}
 				TOut[j] += alpha * Mistakes[j] * Currents[j] * (1 - Currents[j]);
 			}
 
-			for (int j = 0; j < 40; j++)
+			for (int j = 0; j < HIDDEN; j++)
 			{
-				for (int i = 0; i < 20; i++)
+				for (int i = 0; i < INPUT; i++)
 				{
 					W12[i][j] -= alpha * MistakesHid[j] * hidden[j] * (1 - hidden[j]) * Inputs[i];
 				}
@@ -142,7 +144,7 @@ int main()
 	{
 		Inputs = Vectors[i];
 		cout << "Vector - " << i + 1 << " - ";
-		for (int j = 0; j < 20; j++)
+		for (int j = 0; j < INPUT; j++)
 		{
 			cout << Inputs[j] << ' ';
 		}
